deae0f94b8b4c529166c3c0de94a862ad8a1d5aa
Max Robb <maxbenrobb@maxs-mbp.attlocal.net>
2018-03-06 09:23:13 -0500

    minor fixes to terrible code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,11 @@
 
+var incorrect //to store number of answers answered incorrectly like losses++;
+var correct //to store number of correct like wins++;
+var correctAnswer1 //each correct choice needs a variable or ID
+var incorrectAnswer1 //each incorrect choice needs a an ID too
+
+
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -8,18 +15,21 @@ function startTimer(duration, display) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.text(minutes + ":" + seconds);
+        $("#timer").text(minutes + ":" + seconds);
+        //display.text(minutes + ":" + seconds);
+
 
         if (--timer < 0) {
-            timer = duration;
+            alert("game over!") 
+        // if (--timer < 0) {
+        //     timer = duration;
         }
     }, 1000);
 }
 
 jQuery(function ($) {
-    var fiveMinutes = 60 * 5,
+    var thirtySeconds = 30,
         display = $('#time');
-    startTimer(fiveMinutes, display);
+    startTimer(thirtySeconds, display);
 });
 
-$("#timer").text(timer);
